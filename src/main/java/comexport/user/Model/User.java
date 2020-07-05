21ff8c74c15d4c00162904a537a911426e86af29
Fragnul <33350929+Fragnul@users.noreply.github.com>
2020-07-05 18:21:06 -0300

    correção maven

--- src/main/java/comexport/user/Model/User.java
+++ src/main/java/comexport/user/Model/User.java
@@ -10,7 +10,6 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Set;
 
 @Entity
 @Table(name = "user")
--- src/test/java/comexport/user/UserApplicationTests.java
+++ src/test/java/comexport/user/UserApplicationTests.java
@@ -1,5 +1,6 @@
 package comexport.user;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
 import comexport.user.Model.Contato;
