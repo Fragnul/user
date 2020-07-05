6d592166fe2d16d2c1ddb46bb096a653b4f81d27
Fragnul <33350929+Fragnul@users.noreply.github.com>
2020-07-05 19:00:40 -0300

    Commit

--- src/main/java/comexport/user/Repository/ContatoRepository.java
+++
@@ -1,7 +0,0 @@
-package comexport.user.Repository;
-
-import comexport.user.Model.Contato;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContatoRepository  extends JpaRepository<Contato, Long> {
-}
